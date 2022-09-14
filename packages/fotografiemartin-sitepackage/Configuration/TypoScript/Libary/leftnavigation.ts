f975667b49893f2868e035e804eb4b3ae4b27942
omni-primus <56962599+omni-primus@users.noreply.github.com>
2022-09-14 02:51:42 +0200

    add right and left navigation, add icon font, header and footer styling

---
+++ packages/fotografiemartin-sitepackage/Configuration/TypoScript/Libary/leftnavigation.ts
@@ -0,0 +1,18 @@
+## Left Header Navigation
+lib.leftnavigation = HMENU
+lib.leftnavigation {
+    special = directory
+    special.value = 91
+
+    1 = TMENU
+
+    1 {
+
+        NO = 1
+        NO.ATagParams = class="nav-link legal-link inactive"
+
+        CUR = 1
+        CUR.ATagParams = class="nav-link legal-link active"
+    }
+
+}
---
+++ packages/fotografiemartin-sitepackage/Configuration/TypoScript/Libary/rightnavigation.ts
@@ -0,0 +1,18 @@
+## Left Header Navigation
+lib.rightnavigation = HMENU
+lib.rightnavigation {
+    special = directory
+    special.value = 94
+
+    1 = TMENU
+
+    1 {
+
+        NO = 1
+        NO.ATagParams = class="nav-link legal-link inactive"
+
+        CUR = 1
+        CUR.ATagParams = class="nav-link legal-link active"
+    }
+
+}
