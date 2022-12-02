94276966b086c1b543c8a03de824878c4f92182c
omni-primus <56962599+omni-primus@users.noreply.github.com>
2022-12-02 01:39:20 +0100

    create gallery element

--- packages/fotografiemartin-sitepackage/Configuration/TCA/Overrides/tt_content.php
+++ packages/fotografiemartin-sitepackage/Configuration/TCA/Overrides/tt_content.php
@@ -25,3 +25,29 @@ $GLOBALS['TCA']['tt_content']['types']['backgroundBanner'] = [
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
       '
 ];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        // title
+        'Gallery',
+        // plugin signature: extkey_identifier
+        'galleryElement',
+        // icon identifier
+        'my-icon-identifier',
+    ],
+    'textmedia', //keine Ahnung
+    'after' //auch keine Ahnung
+);
+
+$GLOBALS['TCA']['tt_content']['types']['galleryElement'] = [
+    'showitem' => '
+          --div--;General,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+      '
+];
--- packages/fotografiemartin-sitepackage/ext_localconf.php
+++ packages/fotografiemartin-sitepackage/ext_localconf.php
@@ -13,3 +13,7 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fotografiemartin_sitepackage'] = 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fotografiemartin_sitepackage/Configuration/TsConfig/Page/Mod/Wizard/backgroundBanner.tsconfig">'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fotografiemartin_sitepackage/Configuration/TsConfig/Page/Mod/Wizard/galleryElement.tsconfig">'
+);
